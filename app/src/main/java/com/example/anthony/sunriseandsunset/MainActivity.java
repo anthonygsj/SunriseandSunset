e753b8634776f325d24ede3cd0b7495a9eb18457
Anthony <sgao24@illinois.edu>
2018-12-01 20:18:58 -0600

    created UI

--- app/src/main/java/com/example/anthony/sunriseandsunset/MainActivity.java
+++ app/src/main/java/com/example/anthony/sunriseandsunset/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.anthony.sunriseandsunset;
-
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button generate = findViewById(R.id.get);
+        generate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("My App", "Account has been generated");
+                Toast toast = Toast.makeText(getApplicationContext(), "Account Generated", Toast.LENGTH_LONG);
+                toast.show();
+            }
+        });
     }
 }
