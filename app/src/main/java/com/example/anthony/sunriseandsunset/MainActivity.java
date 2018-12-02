d677b1591968233ce04c9b369dd5f36c25ac1d45
Owner <sgao24@illinois.edu>
2018-12-02 00:49:18 -0600

    o

--- app/src/main/java/com/example/anthony/sunriseandsunset/MainActivity.java
+++ app/src/main/java/com/example/anthony/sunriseandsunset/MainActivity.java
@@ -6,46 +6,61 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import org.json.JSONObject;
+import java.net.*;
+import java.io.*;
 
 public class MainActivity extends AppCompatActivity {
-    private String sendGet() throws Exception {
-        String url = "https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400";
-        URL obj = new URL(url);
-        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-        con.setRequestMethod("GET");
-        con.setRequestProperty(null, null);
-        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-        String inputLine;
-        StringBuffer response = new StringBuffer();
-
-        while ((inputLine = in.readLine()) != null) {
-            response.append(inputLine);
-        }
-        in.close();
-
-        return (response.toString());
-    }
+    String url = "https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final TextView textBox = findViewById(R.id.textBox);
-        Button generate = findViewById(R.id.get);
-        generate.setOnClickListener(new View.OnClickListener() {
+        Button get = findViewById(R.id.get);
+
+        get.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                HttpURLConnection connection = null;
+                BufferedReader reader = null;
+
                 try {
-                    textBox.setText(sendGet());
-                    Toast toast = Toast.makeText(getApplicationContext(), "info obtained", Toast.LENGTH_LONG);
-                    toast.show();
-                } catch (Exception e) {
-                    textBox.setText("you messed up");
+                    URL url = new URL("https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400");
+                    connection = (HttpURLConnection) url.openConnection();
+                    connection.connect();
+
+                    InputStream stream = connection.getInputStream();
+
+                    reader = new BufferedReader(new InputStreamReader(stream));
+
+                    StringBuffer buffer = new StringBuffer();
+
+                    String line = "";
+                    while((line = reader.readLine()) != null) {
+                        buffer.append(line);
+                    }
+
+                    textBox.setText(buffer.toString());
+
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    if (connection != null) {
+                        connection.disconnect();
+                    }
+                    try {
+                        if (reader != null) {
+                            reader.close();
+                        }
+                    } catch (IOException e){
+                        e.printStackTrace();
+                    }
                 }
+                Toast toast = Toast.makeText(getApplicationContext(), "info obtained", Toast.LENGTH_LONG);
+                toast.show();
                 Log.i("My App", "Sunrise and Sunset info obtained");
             }
         });
