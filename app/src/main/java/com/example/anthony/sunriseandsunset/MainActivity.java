4f669ce5d93305005b77940702c62014f4f0d724
Owner <sgao24@illinois.edu>
2018-12-02 01:29:47 -0600

    .

--- app/src/main/java/com/example/anthony/sunriseandsunset/MainActivity.java
+++ app/src/main/java/com/example/anthony/sunriseandsunset/MainActivity.java
@@ -1,4 +1,5 @@
 package com.example.anthony.sunriseandsunset;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,57 +13,73 @@ import java.io.*;
 
 public class MainActivity extends AppCompatActivity {
     String url = "https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400";
+    private final TextView textBox = findViewById(R.id.textBox);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final TextView textBox = findViewById(R.id.textBox);
         Button get = findViewById(R.id.get);
 
         get.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                HttpURLConnection connection = null;
-                BufferedReader reader = null;
-
-                try {
-                    URL url = new URL("https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400");
-                    connection = (HttpURLConnection) url.openConnection();
-                    connection.connect();
-
-                    InputStream stream = connection.getInputStream();
-
-                    reader = new BufferedReader(new InputStreamReader(stream));
-
-                    StringBuffer buffer = new StringBuffer();
-
-                    String line = "";
-                    while((line = reader.readLine()) != null) {
-                        buffer.append(line);
-                    }
-
-                    textBox.setText(buffer.toString());
-
-                } catch (MalformedURLException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } finally {
-                    if (connection != null) {
-                        connection.disconnect();
-                    }
-                    try {
-                        if (reader != null) {
-                            reader.close();
-                        }
-                    } catch (IOException e){
-                        e.printStackTrace();
-                    }
-                }
-                Toast toast = Toast.makeText(getApplicationContext(), "info obtained", Toast.LENGTH_LONG);
-                toast.show();
-                Log.i("My App", "Sunrise and Sunset info obtained");
+                new JSONTask().execute("https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400");
             }
         });
+    }
+
+    public class JSONTask extends AsyncTask<String, String, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+            HttpURLConnection connection = null;
+            BufferedReader reader = null;
+
+            try {
+                URL url = new URL(urls[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+
+                InputStream stream = connection.getInputStream();
+
+                reader = new BufferedReader(new InputStreamReader(stream));
+
+                StringBuffer buffer = new StringBuffer();
+
+                String line = "";
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line);
+                }
+
+                return buffer.toString();
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+                try {
+                    if (reader != null) {
+                        reader.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            Toast toast = Toast.makeText(getApplicationContext(), "info obtained", Toast.LENGTH_LONG);
+            toast.show();
+            Log.i("My App", "Sunrise and Sunset info obtained");
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            textBox.setText(result);
+        }
     }
 }
