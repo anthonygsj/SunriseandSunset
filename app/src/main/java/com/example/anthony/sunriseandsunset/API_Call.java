ed92cbf7f8f9672a707f2fd03240073277c1f7fd
Owner <sgao24@illinois.edu>
2018-12-02 18:13:32 -0600

    updated API Call using andi's method, getting networkonmainthreadexception when trying to set textView to the result.

---
+++ app/src/main/java/com/example/anthony/sunriseandsunset/API_Call.java
@@ -0,0 +1,78 @@
+package com.example.anthony.sunriseandsunset;
+import com.google.gson.Gson;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Map;
+
+public class API_Call {
+    public static void main(String[] args) {
+        try {
+            System.out.println(API_Call.call_me());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String call_me() throws Exception {
+        String url = "https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400&date=2017-01-01";
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        // optional default is GET
+        con.setRequestMethod("GET");
+        //add request header
+        con.setRequestProperty("User-Agent", "Mozilla/5.0");
+        int responseCode = con.getResponseCode();
+        System.out.println("\nSending 'GET' request to URL : " + url);
+        System.out.println("Response Code : " + responseCode);
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        String myResponse = response.toString();
+        in.close();
+        //print in String
+        System.out.println(myResponse);
+        Map jsonJavaRootObject1 = new Gson().fromJson(myResponse, Map.class);
+        String result = jsonJavaRootObject1.get("results").toString();
+        System.out.println(result);
+        String sunrise = "Sun Rise Time " + result.substring(9, 19);
+        String sunset = "Sun Set Time " + result.substring(28, 38);
+        String solarnoon = "Solar Noon Time " + result.substring(51, 62);
+        String daylength = "Day Length " + result.substring(75, 83) + " (Hour,min)";
+        String civiltwilightbegin = "Civil Twilight Begins At " + result.substring(106, 116);
+        String civiltwilightend = "Civil Twilight Ends At " + result.substring(137, 147);
+        String naturaltwilightbegins = "Natural Twilight Begins At " + result.substring(173, 183);
+        String naturaltwilightends = "Natural Twilight Ends At " + result.substring(207, 217);
+        String astronomicaltwilightbegin = "Astronomical Twilight Begins At " + result.substring(247, 257);
+        String astronomicaltwilightend = "Astronomical Twilight Ends At " + result.substring(285, 295);
+
+        /*
+        System.out.println(sunrise);
+        System.out.println(sunset);
+        System.out.println(solarnoon);
+        System.out.println(daylength);
+        System.out.println(civiltwilightbegin);
+        System.out.println(civiltwilightend);
+        System.out.println(naturaltwilightbegins);
+        System.out.println(naturaltwilightends);
+        System.out.println(astronomicaltwilightbegin);
+        System.out.println(astronomicaltwilightend);
+         */
+
+        return sunrise + "\n"
+                + sunset + "\n"
+                + solarnoon + "\n"
+                + daylength + "\n"
+                + civiltwilightbegin + "\n"
+                + civiltwilightend + "\n"
+                + naturaltwilightbegins + "\n"
+                + naturaltwilightends + "\n"
+                + astronomicaltwilightbegin + "\n"
+                + astronomicaltwilightend;
+    }
+}
--- app/src/main/java/com/example/anthony/sunriseandsunset/MainActivity.java
+++ app/src/main/java/com/example/anthony/sunriseandsunset/MainActivity.java
@@ -7,79 +7,28 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-import org.json.JSONObject;
-import java.net.*;
-import java.io.*;
 
 public class MainActivity extends AppCompatActivity {
-    String url = "https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400";
-    private final TextView textBox = findViewById(R.id.textBox);
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button get = findViewById(R.id.get);
 
-        get.setOnClickListener(new View.OnClickListener() {
+        Button generate = findViewById(R.id.get);
+        final TextView textBox =findViewById(R.id.textBox);
+        generate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                new JSONTask().execute("https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400");
-            }
-        });
-    }
-
-    public class JSONTask extends AsyncTask<String, String, String> {
-
-        @Override
-        protected String doInBackground(String... urls) {
-            HttpURLConnection connection = null;
-            BufferedReader reader = null;
-
-            try {
-                URL url = new URL(urls[0]);
-                connection = (HttpURLConnection) url.openConnection();
-                connection.connect();
-
-                InputStream stream = connection.getInputStream();
-
-                reader = new BufferedReader(new InputStreamReader(stream));
-
-                StringBuffer buffer = new StringBuffer();
-
-                String line = "";
-                while ((line = reader.readLine()) != null) {
-                    buffer.append(line);
-                }
-
-                return buffer.toString();
-
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            } finally {
-                if (connection != null) {
-                    connection.disconnect();
-                }
-                try {
-                    if (reader != null) {
-                        reader.close();
-                    }
-                } catch (IOException e) {
+                try{
+                    textBox.setText(API_Call.call_me());
+                } catch (Exception e) {
+                    textBox.setText("An error has occured.");
                     e.printStackTrace();
                 }
+                Log.i("My App", "Info Obtained");
+                Toast toast = Toast.makeText(getApplicationContext(), "Info Obtained", Toast.LENGTH_LONG);
+                toast.show();
             }
-            Toast toast = Toast.makeText(getApplicationContext(), "info obtained", Toast.LENGTH_LONG);
-            toast.show();
-            Log.i("My App", "Sunrise and Sunset info obtained");
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-            super.onPostExecute(result);
-            textBox.setText(result);
-        }
+        });
     }
 }
