1d91697e77043c2cea201c3860f05de2969b8cda
thoma <txwei2@illinois.edu>
2018-12-04 16:16:54 -0600

    responding to user input

--- app/src/main/java/com/example/anthony/sunriseandsunset/MainActivity.java
+++ app/src/main/java/com/example/anthony/sunriseandsunset/MainActivity.java
@@ -5,14 +5,18 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         Button generate = findViewById(R.id.get);
         final TextView textBox =findViewById(R.id.textBox);
@@ -30,5 +34,15 @@ public class MainActivity extends AppCompatActivity {
                 toast.show();
             }
         });
+
+    }
+    /** called when user clicks view times button
+     * https://developer.android.com/training/basics/firstapp/starting-activity#CreateActivity*/
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = findViewById(R.id.editText); //im not sure about this line
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
